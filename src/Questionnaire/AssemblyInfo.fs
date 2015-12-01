8b4d879e55f687165221a4973eac1d2e3f96bc9b
Jeremy Abbott <jeremymabbott@gmail.com>
2015-11-30 19:43:37 -0600

    Initial commit

---
+++ src/Questionnaire/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Questionnaire")>]
+[<assembly: AssemblyProductAttribute("Questionnaire")>]
+[<assembly: AssemblyDescriptionAttribute("Build and publish questionnaires for research")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
